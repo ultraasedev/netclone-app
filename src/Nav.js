5723ffb0d151abc98a4ed3dff5423213bc169b96
G.Franck <franckguerin05@gmail.com>
2023-01-22 11:28:21 +0100

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './nav.css';
+
+function Nav() {
+  return (
+    <div className='nav'></div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDc7uea6-hJUDsqMluU9AAE4lOqg_EsXoY",
+    authDomain: "netclone-389ad.firebaseapp.com",
+    projectId: "netclone-389ad",
+    storageBucket: "netclone-389ad.appspot.com",
+    messagingSenderId: "466231307383",
+    appId: "1:466231307383:web:121041805c3fa192ccaa72"
+  };
